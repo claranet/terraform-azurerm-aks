a289e2e0bed380d22589f618cbe163db85e0597a
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-02-13 15:13:20 +0100

    AZ-123 Add id and name of appgw resources in output

--- modules/appgw/outputs.tf
+++ modules/appgw/outputs.tf
@@ -1,3 +1,19 @@
-output "id" {
-  value = "${azurerm_application_gateway.app_gateway.id}"
+output "application_gateway_id" {
+  description = "Application gateway Id"
+  value       = azurerm_application_gateway.app_gateway.id
+}
+
+output "application_gateway_name" {
+  description = "Application gateway name"
+  value       = azurerm_application_gateway.app_gateway.name
+}
+
+output "public_ip_id" {
+  description = "Application gateway public ip Id"
+  value       = azurerm_public_ip.ip.id
+}
+
+output "public_ip_name" {
+  description = "Application gateway public ip name"
+  value       = azurerm_public_ip.ip.name
 }
