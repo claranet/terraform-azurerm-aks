d4074166690b2b35e83ea59272a338613f052788
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2020-09-10 15:47:49 +0200

    AZ-251-Fix versions

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    azurerm    = "~> 2.10.0"
+    azurerm    = ">= 2.10.0"
     helm       = "= 1.1.1"
-    kubernetes = "~> 1.11.1"
+    kubernetes = ">= 1.11.1"
   }
 }
