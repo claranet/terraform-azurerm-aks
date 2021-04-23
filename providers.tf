b573cd94a2279357fe19cbba505519a7af67bd66
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-04-23 10:12:19 +0200

    AZ-483: Remove deprecated option on provider helm

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ provider "kubernetes" {
 provider "helm" {
   alias = "aks-module"
   kubernetes {
-    load_config_file = false
-
     host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
