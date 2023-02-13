af967f1c3a0f90c02ec0d4135aae227fce5b591f
Alex Morozenko <alexmorozenko@gmail.com>
2023-02-13 00:58:51 -0500

    Add more configuration options to cluster's node pools

--- r-aks.tf
+++ r-aks.tf
@@ -35,6 +35,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type                = local.default_node_pool.type
     vnet_subnet_id      = local.default_node_pool.vnet_subnet_id
     node_taints         = local.default_node_pool.node_taints
+    node_labels         = local.default_node_pool.node_labels
     tags                = merge(local.default_tags, var.default_node_pool_tags)
   }
 
@@ -118,22 +119,28 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
-  count                 = length(local.nodes_pools)
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  name                  = local.nodes_pools[count.index].name
-  vm_size               = local.nodes_pools[count.index].vm_size
-  os_type               = local.nodes_pools[count.index].os_type
-  os_disk_type          = local.nodes_pools[count.index].os_disk_type
-  os_disk_size_gb       = local.nodes_pools[count.index].os_disk_size_gb
-  vnet_subnet_id        = local.nodes_pools[count.index].vnet_subnet_id
-  enable_auto_scaling   = local.nodes_pools[count.index].enable_auto_scaling
-  node_count            = local.nodes_pools[count.index].count
-  min_count             = local.nodes_pools[count.index].min_count
-  max_count             = local.nodes_pools[count.index].max_count
-  max_pods              = local.nodes_pools[count.index].max_pods
-  enable_node_public_ip = local.nodes_pools[count.index].enable_node_public_ip
-  zones                 = local.nodes_pools[count.index].zones
-  tags                  = merge(local.default_tags, var.node_pool_tags)
+  count                  = length(local.nodes_pools)
+  kubernetes_cluster_id  = azurerm_kubernetes_cluster.aks.id
+  name                   = local.nodes_pools[count.index].name
+  vm_size                = local.nodes_pools[count.index].vm_size
+  os_type                = local.nodes_pools[count.index].os_type
+  orchestrator_version   = local.nodes_pools[count.index].orchestrator_version
+  os_disk_type           = local.nodes_pools[count.index].os_disk_type
+  os_disk_size_gb        = local.nodes_pools[count.index].os_disk_size_gb
+  priority               = local.nodes_pools[count.index].priority
+  vnet_subnet_id         = local.nodes_pools[count.index].vnet_subnet_id
+  enable_auto_scaling    = local.nodes_pools[count.index].enable_auto_scaling
+  enable_host_encryption = local.nodes_pools[count.index].enable_host_encryption
+  eviction_policy        = local.nodes_pools[count.index].eviction_policy
+  node_count             = local.nodes_pools[count.index].count
+  min_count              = local.nodes_pools[count.index].min_count
+  max_count              = local.nodes_pools[count.index].max_count
+  max_pods               = local.nodes_pools[count.index].max_pods
+  node_labels            = local.nodes_pools[count.index].node_labels
+  node_taints            = local.nodes_pools[count.index].node_taints
+  enable_node_public_ip  = local.nodes_pools[count.index].enable_node_public_ip
+  zones                  = local.nodes_pools[count.index].zones
+  tags                   = merge(local.default_tags, var.node_pool_tags)
 }
 
 # Allow user assigned identity to manage AKS items in MC_xxx RG
