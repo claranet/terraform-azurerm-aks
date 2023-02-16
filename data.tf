94c9cc0381289b6a720f0fed1e08c0f8f0714e46
Thierry Lei <thierry.lei@fr.clara.net>
2023-02-16 09:52:12 +0100

    AZ-1001: Apply suggestions

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "azurerm_subscription" "current" {}
 
 data "azurerm_virtual_network" "aks_vnet" {
-  count = local.is_custom_private_cluster ? 1 : 0
+  count = local.is_custom_dns_private_cluster ? 1 : 0
 
   name                = reverse(split("/", var.vnet_id))[0]
   resource_group_name = split("/", var.vnet_id)[4]
